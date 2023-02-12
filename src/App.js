0f632495fcba5976be37f30c8de3a380412a7e6e
Ujjwal-Gupta-web <ujjgpta9@gmail.com>
2023-02-12 10:48:31 +0530

    cahnges

--- src/App.js
+++ src/App.js
@@ -11,34 +11,34 @@ import Navbar from './components/Navbar';
 import Note from './components/Note';
 
 function App() {
-  let token=(document.cookie).split(';').find((x)=>x.includes('token')).slice(7);
+  let token = (document.cookie).split(';').find((x) => x.includes('token')).slice(7);
   // console.log({token});
   return (
     <div className='App'>
       {/* <Navbar/> */}
       <Switch>
-          <Route exact path="/">
-          <Navbar/>
-            {token?<Home />:<Redirect to='/login'/>}
-          </Route>
-          <Route exact path="/notes">
-            <>
-            <Navbar/>
-            <MyNotes />
-            </>
-          </Route>
-          <Route exact path="/login">
-            <Login />
-          </Route>
-          <Route exact path="/signup">
-            <Signup />
-          </Route>
-          <Route exact path="/notes/:noteid">
+        <Route exact path="/">
+          <Navbar />
+          {token ? <Home /> : <Redirect to='/login' />}
+        </Route>
+        <Route exact path="/notes">
           <>
-            <Navbar/>
-            <Note/>
+            <Navbar />
+            <MyNotes />
           </>
-          </Route>
+        </Route>
+        <Route exact path="/login">
+          {token ? <Redirect to='/' /> : <Login />}
+        </Route>
+        <Route exact path="/signup">
+          {token ? <Redirect to='/' /> : <Signup />}
+        </Route>
+        <Route exact path="/notes/:noteid">
+          <>
+            <Navbar />
+            <Note />
+          </>
+        </Route>
       </Switch>
     </div>
   );
